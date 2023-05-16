8fc5325717102385dcba22105e5f731e3ed4877b
Martin Luo <89254918+MartinJXLuo@users.noreply.github.com>
2023-05-16 12:01:30 +0200

    First

---
+++ 1_Decimal_Octal_Hexadecimal_Binary/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int n1 = 15;        // Decimal
+int n2 = 017;       // Octal
+int n3 = 0X0F;      // Hexadecimal
+int n4 = 0b00001111;// Binary
+
+cout << n1 << endl;
+cout << n2 << endl;
+cout << n3 << endl;
+cout << n4 << endl;
+
+return 0;
+
+}
